969210860619f3c11a1648eda3be4f49a27537fd
Austin Jackson <themacphage@gmail.com>
2020-04-11 01:47:36 -0500

    boring

--- addon/paste.js
+++ addon/paste.js
@@ -1,8 +1,8 @@
 
-// RUN AT: https://bento.mheducation.com/files/smart-factory/ac8c32b7-9b21-4939-832c-863a04585ca0/smart-package/learning-objectives/535236.json
+// RUN AT: https://bento.mheducation.com/files/smart-factory/###/smart-package/learning-objectives/###.json
 
-let data = await (await fetch("https://bento.mheducation.com/files/smart-factory/ac8c32b7-9b21-4939-832c-863a04585ca0/smart-package/learning-objectives.json", {"credentials":"omit","headers":{"accept":"application/json, text/plain, */*","sec-fetch-dest":"empty"},"referrer":"https://learning.mheducation.com/static/awd/index.html?_t=1586575074170","referrerPolicy":"no-referrer-when-downgrade","body":null,"method":"GET","mode":"cors"})).json();
-const BASEURL = 'https://bento.mheducation.com/files/smart-factory/ac8c32b7-9b21-4939-832c-863a04585ca0/smart-package';
+let data = await (await fetch("https://bento.mheducation.com/files/smart-factory/###/smart-package/learning-objectives.json", {"credentials":"omit","headers":{"accept":"application/json, text/plain, */*","sec-fetch-dest":"empty"},"referrer":"https://learning.mheducation.com/static/awd/index.html?_t=1586575074170","referrerPolicy":"no-referrer-when-downgrade","body":null,"method":"GET","mode":"cors"})).json();
+const BASEURL = 'https://bento.mheducation.com/files/smart-factory/###/smart-package';
 const snooze = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 let x = 0;
